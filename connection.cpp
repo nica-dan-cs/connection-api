fda4bd82824e95da0f0691a4cf6b4a724c05aa24
Nica Dan <nica.dan.cs@gmail.com>
2016-12-26 22:50:49 +0000

    Enabled listening for multiple connections at once on a separate thread
    Signed-off-by: Nica Dan <nica.dan.cs@gmail.com>

--- connection.cpp
+++ connection.cpp
@@ -1,5 +1,14 @@
 #include "connection.h"
 
+bool connection_t::is_connected(){
+    return am_i_connected;
+}
+
+void connection_t::assign_connected_fd(int assigned_fd){
+    socket_fd = assigned_fd;
+    am_i_connected = true;
+}
+
 bool connection_t::create_socket(int* file_descriptor){
     *file_descriptor = socket(AF_INET,SOCK_STREAM,0);
     if (*file_descriptor == -1){
@@ -44,9 +53,11 @@ bool connection_t::connect_to(const char* ip_address,unsigned short port){
         std::cerr << "connect() failed\n";
         return false;
     }
+
+    am_i_connected = true;
     return true;
 }
-bool connection_t::listen_on(unsigned short port){
+bool connection_t::await_connection_on(unsigned short port){
     struct sockaddr_in this_side;
 
     int liste_socket_fd;
@@ -72,6 +83,7 @@ bool connection_t::listen_on(unsigned short port){
         return false;
     }
 
+    am_i_connected = true;
     return true;
 }
 
@@ -98,33 +110,6 @@ void connection_t::receive_as(struct_type** output_buffer){
     delete receive_buffer;
 }
 
-bool connection_t::poll(connection_t** connections,unsigned short connections_count,unsigned short timeout_useconds,bool* output_buffer){
-    struct pollfd* poll_fds = new pollfd[connections_count];
-
-    for(int i=0; i < connections_count; i++){
-        poll_fds[i].fd = connections[i]->socket_fd;
-        poll_fds[i].events = 0;
-        poll_fds[i].events |= POLLIN;
-        poll_fds[i].revents = 0;
-    }
-
-    int poll_status = ::poll(poll_fds,connections_count,timeout_useconds);
-
-    if(poll_status == -1){
-        std::cerr << "poll() failed\n";
-        return false;
-    }
-
-    for(int i=0; i< connections_count; i++){
-        if(poll_fds[i].revents == POLLIN)
-            output_buffer[i] = true;
-        else
-            output_buffer[i] = false;
-    }
-
-    return true;
-}
-
 bool connection_t::poll(connection_t* connections,unsigned short connections_count,unsigned short timeout_useconds,bool* output_buffer){
     struct pollfd* poll_fds = new pollfd[connections_count];
 
@@ -143,6 +128,11 @@ bool connection_t::poll(connection_t* connections,unsigned short connections_cou
     }
 
     for(int i=0; i< connections_count; i++){
+        if(!connections[i].is_connected()){
+            output_buffer[i] = false;
+            break;
+        }
+
         if(poll_fds[i].revents == POLLIN)
             output_buffer[i] = true;
         else
@@ -150,4 +140,64 @@ bool connection_t::poll(connection_t* connections,unsigned short connections_cou
     }
 
     return true;
+}
+
+void* __await_multiple_connections(void* input){
+    
+    connection_t* connections = ((listen_info_t*)input)->connections;
+    unsigned short connections_count = ((listen_info_t*)input)->connections_count;
+    unsigned short port = ((listen_info_t*)input)->port;
+    
+    int listen_socket_fd = socket(AF_INET,SOCK_STREAM,0);
+    if(listen_socket_fd == -1){
+        std::cerr << "socket() failed\n";
+        return nullptr;
+    }
+
+    struct sockaddr_in this_side;
+    this_side.sin_family = AF_INET;
+    this_side.sin_addr.s_addr = INADDR_ANY;
+    this_side.sin_port = htons(port);
+     
+    int bind_status = bind(listen_socket_fd,(struct sockaddr *)&this_side,sizeof(this_side));
+
+    if(bind_status == -1){
+        std::cerr << "bind() failed\n";
+        return nullptr;
+    }
+
+    unsigned short unresolved_connections = connections_count;
+
+    while(unresolved_connections != 0){
+        connection_t* unused_connection = connections;
+        while(unused_connection->is_connected())
+            unused_connection++;
+
+        listen(listen_socket_fd , unresolved_connections);
+
+        socklen_t struct_sizeof = sizeof(struct sockaddr_in);
+        int accepted_fd = accept(listen_socket_fd,(struct sockaddr *)&this_side,(socklen_t*)&struct_sizeof);
+        unused_connection->assign_connected_fd(accepted_fd);
+        unresolved_connections--;
+    }
+
+    return nullptr;
+}
+
+pthread_t* connection_t::await_multiple_connections(connection_t* connections,unsigned short connections_count,unsigned short port){
+
+    struct listen_info_t *listen_info = new listen_info_t;
+    listen_info->connections = connections;
+    listen_info->connections_count = connections_count;
+    listen_info->port = port;
+
+    pthread_t* listen_thread = new pthread_t;
+    int creation_status = pthread_create(listen_thread,NULL,__await_multiple_connections,(void*)listen_info);
+
+    if(creation_status){
+        std::cerr << "pthread_create() failed with error code " << creation_status << "\n";
+        return nullptr;
+    }
+
+    return listen_thread;
 }
--- connection.h
+++ connection.h
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <poll.h>
+
+// /void __await_multiple_connections(void* input);
 
 class connection_t{
     private:
         int socket_fd;
+        bool am_i_connected = false;
         bool create_socket(int* file_descriptor);
 
     public:
+        bool is_connected();
+        void assign_connected_fd(int assigned_fd);
 
         bool send(void* buffer,size_t buffer_length);
         bool receive(void** buffer,size_t buffer_length);
 
         bool connect_to(const char* ip_address,unsigned short port);
-        bool listen_on(unsigned short port);
+        bool await_connection_on(unsigned short port);
 
         template <class struct_type>
         void send_as(struct_type* input);
         template <class struct_type>
         void receive_as(struct_type** output_buffer);
 
-        static bool poll(connection_t** connections,unsigned short connections_count,unsigned short timeout_useconds,bool* output_buffer);
         static bool poll(connection_t* connections,unsigned short connections_count,unsigned short timeout_useconds,bool* output_buffer);
+        static pthread_t* await_multiple_connections(connection_t* connections,unsigned short connections_count,unsigned short port);
+};
+
+struct listen_info_t{
+    connection_t* connections;
+    unsigned short connections_count;
+    unsigned short port;
 };
